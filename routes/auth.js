9ea742d4d81ab55903dc2c096a147dfe90f80937
ing-jose-rafael <ing.jcarreno@gmail.com>
2021-10-26 09:34:03 -0500

    middleware ruta auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const { Router } = require('express'); // para poder crear la constante router
+const { check } = require('express-validator');
 const { login } = require('../controllers/auth');
+const { validarCampos } = require('../middleware/validar-campos');
 
 const router = Router();
 
-router.post('/login',login);
+router.post('/login',[
+    check('correo','El correo es obligatorio').notEmpty(),
+    check('correo','El correo no válido').isEmail(),
+    check('password','El correo es obligatorio').notEmpty(),
+    validarCampos
+],login);
 
 module.exports = router;
