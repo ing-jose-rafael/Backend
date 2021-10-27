191356e0e7c8a50c28ff439ba6405fa777943b15
ing-jose-rafael <ing.jcarreno@gmail.com>
2021-10-27 10:07:30 -0500

    index models

--- models/index.js
+++ models/index.js
@@ -1,10 +1,11 @@
 const Role = require('./role');
 const Usuario = require('./usuario');
 const Server = require('./server');
+const Categoria= require('./categoria');
 
 module.exports={
- Role,
- Usuario,
- Server,
-
+    Categoria,
+    Role,
+    Usuario,
+    Server,
 }
